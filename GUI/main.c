f3c545b639b5ebf9fbe4fd8c02536a7df9983227
Razvan Rus <razvanrus2003@gmail.com>
2023-05-16 10:16:59 +0300

    First button

--- GUI/main.c
+++ GUI/main.c
@@ -1,23 +1,53 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
 
 void myCSS(void);
+
+static void myprint(GtkWidget *button, gpointer data)
+{
+    printf("Playing...\n");
+}
+
+void myWindow(GtkWidget **window)
+{
+    gtk_window_set_default_size(GTK_WINDOW(*window), 1200, 800);
+
+    gtk_window_set_position(GTK_WINDOW(*window), GTK_WIN_POS_CENTER);
+
+    gtk_widget_set_name(GTK_WIDGET(*window), "myWindow");
+
+    gtk_widget_show_all(*window);
+
+    gtk_window_set_title(GTK_WINDOW(*window), "Spotify");
+
+    gtk_window_set_resizable(GTK_WINDOW(*window), FALSE);
+}
+
+void myButton(GtkWidget **button)
+{
+    *button = gtk_button_new_with_label("Play");
+
+    gtk_widget_set_name(GTK_WIDGET(*button), "Play-Pause");
+
+    g_signal_connect(*button, "clicked", G_CALLBACK(myprint), NULL);
+}
 
 static void activate(GtkApplication *app, gpointer user_data)
 {
     // gtk code
-    GtkWidget *window;
+    GtkWidget *window, *button, *fixed;
 
     window = gtk_application_window_new(app);
 
-    gtk_window_set_default_size(GTK_WINDOW(window), 1200, 800);
+    fixed = gtk_fixed_new();
 
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    myButton(&button);
 
-    gtk_widget_set_name(GTK_WIDGET(window), "myWindow");
+    gtk_fixed_put(GTK_FIXED(fixed), button, 575, 700);
 
-    gtk_widget_show_all(window);
+    gtk_container_add(GTK_CONTAINER(window), fixed);
 
-    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+    myWindow(&window);
 }
 
 int main(int argc, char **argv)
@@ -30,7 +60,6 @@ int main(int argc, char **argv)
     app = gtk_application_new("in.music", G_APPLICATION_FLAGS_NONE);
 
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-
     ret = g_application_run(G_APPLICATION(app), argc, argv);
 
     g_object_unref(app);
--- Model/start.c
+++ Model/start.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
+    printf("%d\n", 10);
     gtk_widget_show_all(window);
 
     gtk_main ();
